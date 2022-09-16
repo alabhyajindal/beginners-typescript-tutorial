1320afd38ebdd6961c6c1d81fba3c9f462f7b6e7
Alabhya Jindal <alabhya10@gmail.com>
2022-09-16 22:01:28 +0530

    02

--- scripts/exercise.js
+++ scripts/exercise.js
@@ -1,29 +1,29 @@
-const { execSync } = require("child_process");
-const fs = require("fs");
-const path = require("path");
-const chokidar = require("chokidar");
+const { execSync } = require('child_process');
+const fs = require('fs');
+const path = require('path');
+const chokidar = require('chokidar');
 
-const srcPath = path.resolve(__dirname, "../src");
-const tsconfigPath = path.resolve(__dirname, "../tsconfig.json");
+const srcPath = path.resolve(__dirname, '../src');
+const tsconfigPath = path.resolve(__dirname, '../tsconfig.json');
 
 const [, , exercise] = process.argv;
 
 if (!exercise) {
-  console.log("Please specify an exercise");
+  console.log('Please specify an exercise');
   process.exit(1);
 }
 
 const allExercises = fs.readdirSync(srcPath);
 
-let pathIndicator = ".problem.";
+let pathIndicator = '.problem.';
 
 if (process.env.SOLUTION) {
-  pathIndicator = ".solution.";
+  pathIndicator = '.solution.';
 }
 
 const exercisePath = allExercises.find(
   (exercisePath) =>
-    exercisePath.startsWith(exercise) && exercisePath.includes(pathIndicator),
+    exercisePath.startsWith(exercise) && exercisePath.includes(pathIndicator)
 );
 
 if (!exercisePath) {
@@ -34,24 +34,26 @@ if (!exercisePath) {
 const exerciseFile = path.resolve(srcPath, exercisePath);
 
 // One-liner for current directory
-chokidar.watch(exerciseFile).on("all", (event, path) => {
-  const fileContents = fs.readFileSync(exerciseFile, "utf8");
+chokidar.watch(exerciseFile).on('all', (event, path) => {
+  const fileContents = fs.readFileSync(exerciseFile, 'utf8');
 
-  const containsVitest = fileContents.includes("vitest");
+  const containsVitest = fileContents.includes('vitest');
   try {
     console.clear();
     if (containsVitest) {
-      console.log("Running tests...");
+      console.log('Running tests...');
       execSync(`vitest run "${exerciseFile}" --passWithNoTests`, {
-        stdio: "inherit",
+        stdio: 'inherit',
       });
     }
-    console.log("Checking types...");
+    console.log('Checking types...');
     execSync(`tsc "${exerciseFile}" --noEmit --strict`, {
-      stdio: "inherit",
+      stdio: 'inherit',
     });
-    console.log("Typecheck complete. You finished the exercise!");
+    console.log('Typecheck complete. You finished the exercise!');
   } catch (e) {
-    console.log("Failed. Try again!");
+    console.log('Failed. Try again!');
+  } finally {
+    process.exit();
   }
 });
